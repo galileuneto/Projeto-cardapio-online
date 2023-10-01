626f56622a634c40bdf66f900fe38a61b6a2e09f
Galileu Neto <141690464+galileuneto@users.noreply.github.com>
2023-10-01 20:39:32 -0300

    atualizado

--- CardapioOnline/script.js
+++ CardapioOnline/script.js
@@ -1,13 +1,36 @@
-var abrirMenu =  document.getElementById("abrir-menu");
-var fecharMenu =  document.getElementById("fechar-menu");
+var header = document.getElementById('header');
+var navigationHeader = document.getElementById('navigation_header');
+var content = document.getElementById('content');
+var showSidebar = false;
 
 
+function abrir() {
+    showSidebar = !showSidebar
+    if (showSidebar == true) {
+        navigationHeader.style.marginLeft = '0vw'
+        navigationHeader.style.animationName = 'showSidebar';
+        content.style.filter = 'blur(2px)';
 
+    } else {
+        navigationHeader.style.marginLeft = '-100vw'
+        navigationHeader.style.animationName = '';
+        content.style.filter = '';
 
-
-
-
-
-function fechar() {
-    if ()
+    }
 }
+function closeSidebar()
+    {
+        if(showSidebar)
+        {
+            showSidebar = true;
+            abrir();
+        }
+    }
+
+    window.addEventListener('resize', function(event) {
+        if(window.innerWidth > 768 && showSidebar) 
+        {  
+            showSidebar = true;
+            abrir();
+        }
+    });
